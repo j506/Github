7835f732c8b422a7b190fb9014d91942393dd623
jagannath moharana <jagannathmoharana96@gmail.com>
2021-04-14 06:00:50 +0530

    secound commit

--- src/main/java/com/pages/loginPage.java
+++ src/main/java/com/pages/loginPage.java
@@ -4,6 +4,8 @@ public class loginPage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("this is the loginpage method");
+		System.out.println("this is the changed method");
 
 	}
 
